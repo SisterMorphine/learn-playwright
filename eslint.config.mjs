cc46951a613e60c06bf4477b9284fbed38bc0589
Leticia Rocha Gonzalez <>
2026-07-09 23:23:10 +0200

    Config for eslint-plugin-playwright

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,15 @@ export default defineConfig([
   globalIgnores(["**/*.js", "**/*.cjs", "**/*.mjs"]),
   { files: ["**/*.{ts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['tests/**/*.ts'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/no-wait-for-timeout': 'error',
+      'playwright/missing-playwright-await': 'error',
+      'playwright/prefer-web-first-assertions': 'error',
+      'playwright/no-force-option': 'warn',
+    }
+  }
 ]);
